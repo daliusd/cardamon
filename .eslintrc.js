f1b5fbefdf051e78444db9abc352890c4635a0c2
Dalius Dobravolskas <dalius.dobravolskas@gmail.com>
2018-12-29 12:19:37 +0200

    Initial version :tada:

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    env: {
+        es6: true,
+        node: true,
+    },
+    extends: 'eslint:recommended',
+    parser: 'babel-eslint',
+    parserOptions: {
+        ecmaVersion: 2015,
+    },
+    rules: {
+        indent: ['error', 4],
+        'linebreak-style': ['error', 'unix'],
+        quotes: ['error', 'single'],
+        semi: ['error', 'always'],
+    },
+};
