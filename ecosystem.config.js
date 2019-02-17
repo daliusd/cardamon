d1b4c245e06953a47e6fa0b910c8a824119ebe42
Dalius Dobravolskas <dalius.dobravolskas@gmail.com>
2019-02-17 15:05:13 +0200

    Newer version.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,12 @@
 module.exports = {
     apps : [{
-        name      : 'Cardamon',
+        name      : 'cardamon',
         script    : 'bin/www',
+
+        instances: 1,
+        autorestart: true,
+        watch: false,
+        max_memory_restart: '500M',
         env: {
             NODE_ENV: 'development',
         },
