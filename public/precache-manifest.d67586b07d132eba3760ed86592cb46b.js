f76b2adf3c3d150214f112c2de3dcf670a3c213b
Dalius Dobravolskas <dalius.dobravolskas@gmail.com>
2019-02-17 13:00:10 +0200

    Optimized client build added.

---
+++ public/precache-manifest.d67586b07d132eba3760ed86592cb46b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "f7f9443cacc64503994fbacb023ffc70",
+    "url": "/static/media/text.f7f9443c.svg"
+  },
+  {
+    "revision": "f3e2a3bc2d08025e7b9819a56487afa2",
+    "url": "/static/media/rotate.f3e2a3bc.svg"
+  },
+  {
+    "revision": "c197425452ab165f43f781035065c972",
+    "url": "/static/media/resize.c1974254.svg"
+  },
+  {
+    "revision": "60f0926cc9035d2e9b420ccff93e8b60",
+    "url": "/static/media/image.60f0926c.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b4e115da2c49f6a473d1",
+    "url": "/static/js/main.b4e115da.chunk.js"
+  },
+  {
+    "revision": "a524219730ad6a5834d4",
+    "url": "/static/js/1.a5242197.chunk.js"
+  },
+  {
+    "revision": "b4e115da2c49f6a473d1",
+    "url": "/static/css/main.c0a74fb0.chunk.css"
+  },
+  {
+    "revision": "0d65dcd717566afc5ad0be04e5480ac0",
+    "url": "/index.html"
+  }
+];
