bf34eaea3f202bdc545e6e9f54316464d7295bbf
Dalius Dobravolskas <dalius.dobravolskas@gmail.com>
2020-07-17 19:57:27 +0300

    Upgrade to v20200717.

---
+++ public/precache-manifest.23c83671bf37f78a6f39a208f76536eb.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de77bab7feea676502c77732f79da427",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f802458bf7db04fb9c8a",
+    "url": "/static/css/2.6fbf90d6.chunk.css"
+  },
+  {
+    "revision": "33b7c6a9c33d4a6a3b3b",
+    "url": "/static/css/main.f991bf11.chunk.css"
+  },
+  {
+    "revision": "f802458bf7db04fb9c8a",
+    "url": "/static/js/2.ed193bb8.chunk.js"
+  },
+  {
+    "revision": "e1c44550587b8c2122e9d1f2587d2713",
+    "url": "/static/js/2.ed193bb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33b7c6a9c33d4a6a3b3b",
+    "url": "/static/js/main.902da2f8.chunk.js"
+  },
+  {
+    "revision": "70ff4fd2c3386c33d371",
+    "url": "/static/js/runtime-main.c285d15e.js"
+  },
+  {
+    "revision": "60f0926cc9035d2e9b420ccff93e8b60",
+    "url": "/static/media/image.60f0926c.svg"
+  },
+  {
+    "revision": "ff30331901ed378dda54985016e0820a",
+    "url": "/static/media/pan.ff303319.svg"
+  },
+  {
+    "revision": "c1fb8dc311851cd568d081945c7eabcc",
+    "url": "/static/media/resize.c1fb8dc3.svg"
+  },
+  {
+    "revision": "4a37bc7be0725f480904aa84fcc02169",
+    "url": "/static/media/rotate.4a37bc7b.svg"
+  },
+  {
+    "revision": "ec7615c528e686b6f12ebc33b0d1c53d",
+    "url": "/static/media/screenshot.ec7615c5.png"
+  },
+  {
+    "revision": "f7f9443cacc64503994fbacb023ffc70",
+    "url": "/static/media/text.f7f9443c.svg"
+  },
+  {
+    "revision": "2d0b374b64735d27880c7612c4366fa6",
+    "url": "/static/media/zoom.2d0b374b.svg"
+  }
+]);
